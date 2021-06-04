a79eb99616cf5a4390925f5608c825191724e318
Surendra Choudary Nettam <nettem.surendra@gmail.com>
2021-06-04 17:33:25 +0930

    adding Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+function Navbar() {
+    const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
+
+    return (
+        <Nav variant="pills" activeKey="1" onSelect={handleSelect}>
+            <Nav.Item>
+                <Nav.Link eventKey="1" href="#/home"> Home </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="2" title="Item"> About </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="3" > Portfolio </Nav.Link>
+            </Nav.Item>
+            
+            <Nav.Item>
+                <Nav.Link eventKey="3" > Contact  </Nav.Link>
+            </Nav.Item>
+        </Nav>
+    );
+}
+
+
+
+export default Navbar
